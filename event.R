665a387e2503e37e444195ee43b389d25135fcf2
Sophiixy <94802301+sophiiixy@users.noreply.github.com>
2022-03-22 17:46:26 -0400

    Update event.R

--- event.R
+++ event.R
@@ -1,133 +1,91 @@
 library(readxl)
 library(tidyverse)
 
-event <- read.csv("C:/Users/sophi/Desktop/81244710872 - Attendee Report (1).csv")
+#### fill in the directory and file name
+event <- read.csv(".csv")
 
-
+# produce histogram for time in session
 hist(event$Time.in.Session..minutes., main="Histogram for Time in Event",
      xlab = "Time in Event (min)", ylab = "Frequency of attendees")
 summary(event$Time.in.Session..minutes.)
 
-
+# produce pie chart for attended vs. not attended students
 df <- data.frame(
   group = c("Attended", "Not Attended"),
-  value = c(34, 66)
+#### fill in the number
+  value = c(, )
 )
 
 bp<- ggplot(df, aes(x="", y=value, fill=group))+
   geom_bar(width = 1, stat = "identity")
-bp
 
 pie <- bp + coord_polar("y", start=0)
-pie
 
 pie + scale_fill_brewer("Blues") + 
   theme(axis.text.x=element_blank())+
   theme_bw() 
 
 
+# produce bar chart to show which channels attendees hear about this event from
+tab <- as.data.frame(table(event$How.did.you.hear.about.this.event.))
 
-tab <- as.data.frame(table(event$How.did.you.hear.about.this.event.,event$University))
-tab$Prop <- round(tab$Freq / 111 * 100,1)
+tab$Prop <- round(tab$Freq / nrow(event) * 100,1)
 colnames(tab)[1] <- "How did you hear about this event?"
-colnames(tab)[2] <- "University"
 
 ggplot(data=tab, aes(x= reorder(`How did you hear about this event?`, Prop)), 
-                                 y=Prop,
-                                 group=University,
-                                 fill=University) +
+                                 y=Prop) +
   geom_bar(stat="identity", aes(y=Prop)) +
   coord_flip() +
   ylim(0,100) +
-  #  geom_text(aes(label=Prop),hjust=-0.3)+
   xlab("How did you hear about this event?")+
   ylab("Proportion (%) of attendees responded")+
   theme_bw() +
   theme(text = element_text(size = 20)) +
- # scale_color_manual(values = c("#a4c639", "#007AFF")) +
- # scale_fill_manual("Device Type", values = c("Android"="#a4c639", "iOS"="#007AFF"))+
   theme(legend.position="bottom")
 
-
+# produce bar chart to show which university attended by attendees 
 tab2 <- as.data.frame(table(event$University))
-tab2$Prop <- round(tab2$Freq / 111 * 100,1)
+tab2$Prop <- round(tab2$Freq / nrow(event) * 100,1)
 colnames(tab2)[1] <- "University"
 
 ggplot(data=tab2, aes(x= reorder(University, Prop)), 
-       y=Prop,
-       group=University,
-       fill=University) +
+       y=Prop) +
   geom_bar(stat="identity", aes(y=Prop)) +
   coord_flip() +
   ylim(0,100) +
-  #  geom_text(aes(label=Prop),hjust=-0.3)+
   xlab("University")+
   ylab("Proportion (%) of attendees")+
   theme_bw() +
   theme(text = element_text(size = 20)) +
-  # scale_color_manual(values = c("#a4c639", "#007AFF")) +
-  # scale_fill_manual("Device Type", values = c("Android"="#a4c639", "iOS"="#007AFF"))+
   theme(legend.position="bottom")
 
-
-
-tab2 <- as.data.frame(table(event$University))
-tab2$Prop <- round(tab2$Freq / 111 * 100,1)
-colnames(tab2)[1] <- "University"
-
-ggplot(data=tab2, aes(x= reorder(University, Prop)), 
-       y=Prop,
-       group=University,
-       fill=University) +
-  geom_bar(stat="identity", aes(y=Prop)) +
-  coord_flip() +
-  ylim(0,100) +
-  #  geom_text(aes(label=Prop),hjust=-0.3)+
-  xlab("University")+
-  ylab("Proportion (%) of attendees")+
-  theme_bw() +
-  theme(text = element_text(size = 20)) +
-  # scale_color_manual(values = c("#a4c639", "#007AFF")) +
-  # scale_fill_manual("Device Type", values = c("Android"="#a4c639", "iOS"="#007AFF"))+
-  theme(legend.position="bottom")
-
-
-
-
-
-
-df <- data.frame(
+# produce pie chart for attendee professional
+df2 <- data.frame(
   group = c("Alumnus/Professional", "MSc", "PharmD",
             "PhD","Postdoctoral Fellow",
             "Undergraduate"),
   value = c(15, 25,4,41,6,7 )
 )
 
-bp<- ggplot(df, aes(x="", y=value, fill=group))+
+bp2 <- ggplot(df2, aes(x="", y=value, fill=group))+
   geom_bar(width = 1, stat = "identity")
-bp
 
-pie <- bp + coord_polar("y", start=0)
-pie
+pie2 <- bp2 + coord_polar("y", start=0)
 
+# select a colour-blind-friendly colour scheme 
 cbPalette <- c("#999999", "#56B4E9","#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-pie  +  scale_fill_manual(values=cbPalette)+
+pie2  +  scale_fill_manual(values=cbPalette)+
   theme(axis.text.x=element_blank())+
   theme_bw() 
 
-library(tidyverse)
-
-
-
-  
+# produce a world map to visulize where attendees were joining from
 `Geolocation of attendees` <- c("Canada", "US", "India")
 `Proportion of attendees` <- c(96, 3, 1)
-longitude <- c(-96,	-100,77)
+longitude <- c(-96,-100,77)
 latitude <- c(54, 40,20)
 	
-
-df <- data.frame(`Geolocation of attendees`, `Proportion of attendees`,longitude, latitude)
+df3 <- data.frame(`Geolocation of attendees`, `Proportion of attendees`,longitude, latitude)
 
 world <- map_data("world")
 
@@ -137,7 +95,7 @@ ggplot() +
     aes(long, lat, map_id = region)) +
   theme_bw() +
   geom_point(
-    data = df,
+    data = df3,
     aes(longitude, latitude, 
         color = `Geolocation of attendees`,
         size=`Proportion of attendees`),
